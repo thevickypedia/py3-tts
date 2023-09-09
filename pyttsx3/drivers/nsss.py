5564d38641c8fa5f1e4fd1d376c0087beee022f6
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-09-09 09:10:47 -0500

    Bug fix on macOS Terminate NSApp before endLoop

--- pyttsx3/drivers/nsss.py
+++ pyttsx3/drivers/nsss.py
@@ -2,8 +2,26 @@
 from AppKit import NSSpeechSynthesizer
 from Foundation import *
 from PyObjCTools import AppHelper
+# noinspection PyProtectedMember
+from PyObjCTools.AppHelper import PyObjCAppHelperRunLoopStopper
 
 from ..voice import Voice
+
+
+# noinspection PyUnresolvedReferences
+class RunLoopStopper(PyObjCAppHelperRunLoopStopper):
+    """
+    Overrides PyObjCAppHelperRunLoopStopper to terminate after endLoop.
+    """
+
+    def __init__(self):
+        self.shouldStop = False
+
+    def init(self):
+        return objc.super(RunLoopStopper, self).init()
+
+    def stop(self):
+        self.shouldStop = True
 
 
 # noinspection PyPep8Naming
@@ -42,9 +60,22 @@ class NSSpeechDriver(NSObject):
         self._proxy.setBusy(False)
 
     def startLoop(self):
+        # https://github.com/ronaldoussoren/pyobjc/blob/mater/pyobjc-framework-Cocoa/Lib/PyObjCTools/AppHelper.py#L243C25-L243C25  # noqa
         NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-            0.0, self, 'onPumpFirst:', None, False)
-        AppHelper.runConsoleEventLoop()
+            0.0, self, 'onPumpFirst:', None, False
+        )
+        runLoop = NSRunLoop.currentRunLoop()
+        stopper = RunLoopStopper.alloc().init()
+        PyObjCAppHelperRunLoopStopper.addRunLoopStopper_toRunLoop_(stopper, runLoop)
+        while stopper.shouldRun():
+            nextfire = runLoop.limitDateForMode_(NSDefaultRunLoopMode)
+            soon = NSDate.dateWithTimeIntervalSinceNow_(0)  # maxTimeout in runConsoleEventLoop
+            if nextfire is not None:
+                nextfire = soon.earlierDate_(nextfire)
+            if not runLoop.runMode_beforeDate_(NSDefaultRunLoopMode, nextfire):
+                stopper.stop()
+                break
+        PyObjCAppHelperRunLoopStopper.removeRunLoopStopperFromRunLoop_(runLoop)
 
     @staticmethod
     def endLoop():
